84ae351a23a8f6339783eeeec8587650a33a0ef7
kogantimounika <kogantimounica@gmail.com>
2020-01-10 01:43:11 +0000

    get the details of all reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -81,8 +81,26 @@ const pool = new Pool({
   * @return {Promise<[{}]>} A promise to the reservations.
   */
  const getAllReservations = function(guest_id, limit = 10) {
-   return getAllProperties(null, 2);
- }
+   return pool.query(`
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `, [guest_id,limit]
+   )
+   .then(res => {
+     if(res.rows.length) {
+       return res.rows;
+     }
+     return null;
+   });
+
+
+ };
  exports.getAllReservations = getAllReservations;
 
 /// Properties
